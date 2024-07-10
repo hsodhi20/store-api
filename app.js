5844a18287c805d44ce75223df09596e1ded69cb
hsodhi20 <hsodhi2003@gmail.com>
2024-07-10 20:38:18 +0530

    connect with the database

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
 
 app.get("/", (req, res) => {
   res.send(
@@ -18,6 +19,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening at the port ${port}`));
   } catch (error) {}
 };
