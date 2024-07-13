87b51f04943c808614a37d5327e1bfcf8e036c22
hsodhi20 <hsodhi2003@gmail.com>
2024-07-13 18:10:06 +0530

    write the logic for the controller of products

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 require("dotenv").config();
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 app.get("/", (req, res) => {
   res.send(
     '<h1>Store API</h1> <a href = "/api/v1/products"> products route </a>'
   );
 });
+
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
